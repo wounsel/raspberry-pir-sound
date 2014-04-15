56790b947dcdbcdf5375dbe362f8778ba57123ee
Evan Lee <wounsel@gmail.com>
2014-04-14 22:41:20 -0500

    Create pi_sound.sh
    Shell script used for loading upon boot.

---
+++ pi_sound.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+#! /etc/init.d/pi_sound
+
+case "$1" in
+  start)
+    echo "Starting Raspberry Pi Sound Script"
+    # run application you want to start
+    sudo mount /dev/sda1 /mnt/usbdrive
+    sudo python2 /mnt/usbdrive/pir_sound.py
+    ;;
+  stop)
+    echo "Stopping Raspberry Pi Sound Script"
+    # kill application you want to stop
+    killall python2
+    ;;
+  *)
+    echo "Usage: /etc/init.d/pi_sound {start|stop}"
+    exit 1
+    ;;
+esac
+
+exit 0
+
+
